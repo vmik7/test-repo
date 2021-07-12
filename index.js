e57bf6eab6c289e0b836b34a5cc27f662acd0a02
vmik7 <vmik7@yandex.ru>
2021-07-12 17:40:03 +0300

    Create the test application

---
+++ index.js
@@ -0,0 +1,11 @@
+const signale = require("signale");
+
+signale.note("This is a note");
+signale.success("This is a success");
+signale.start("This is a start");
+signale.await("This is an await");
+signale.complete("This is a complete");
+signale.star("This is a star");
+signale.debug("This is a debug");
+signale.error("This is an error");
+signale.fatal("This is a fatal");
